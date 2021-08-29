57b355f01d764749b2c3f3e7e7637ee9efa841f4
ferasezzaldeen <ferasezaldeen@gmail.com>
2021-08-29 16:36:49 +0300

    finish lab26

--- snacks/urls.py
+++ snacks/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from .views import AboutpageView, HomepageView 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('snacks.urls')),
+    path('',HomepageView.as_view(),name='home'),
+    path('about/',AboutpageView.as_view(),name='about'),
 ]
